e5ece3a8c6e9c8b35b6a07405041e9a6106c9684
smirea <steven.mirea@gmail.com>
2014-02-02 00:11:04 +0200

    updated

--- js/contentscript.js
+++ js/contentscript.js
@@ -2,75 +2,6 @@
 var windows = null;
 var tabs = null;
 
-var scripts = {
-  /**
-   * Performs fuzzy searching on the tabs and returns
-   * @param  {Array} tabs
-   * @param  {String} str
-   * @return {Array}
-   */
-  find: function (tabs, str) {
-    str = str || '';
-    return tabs.map(function (tab) {
-      var res = {
-        title: tab.title.fuzzySearch(str, true),
-        url: tab.url.fuzzySearch(str, true),
-      };
-      for (var key in res) { res[key + 'Score'] = fuzzyScore(res[key]); }
-      res.tab = tab;
-      return res;
-    }).filter(function (match) {
-      return match.title !== null || match.url !== null;
-    }).map(function (match) {
-      match.score = Math.min(match.titleScore, match.urlScore * 0.5);
-      return match;
-    }).sort(function (a, b) {
-      return a.score - b.score;
-    }).map(function (match) {
-      return match.tab;
-    });
-  },
-
-  /**
-   * Sorts the given tabs
-   * @param  {Array} tabs
-   * @return {Array}
-   */
-  sort: function (tabs) {
-    var opt = get_options(cloneArray(arguments).slice(1), {
-      sortKey: ['url', 'title'],
-      order: ['asc', 'desc'],
-    });
-
-    tabs.sort(function (a, b) {
-      return  (a[opt.sortKey] < b[opt.sortKey] && -1) ||
-              (a[opt.sortKey] > b[opt.sortKey] && 1) ||
-              0;
-    });
-
-    opt.order == 'desc' && tabs.reverse();
-
-    return tabs;
-  },
-
-  /**
-   * Randomizes the order of the tabs.
-   * @param  {Array} tabs
-   * @return {Array}
-   */
-  shuffle: function (tabs) {
-    return tabs.shuffle();
-  },
-
-  /**
-   * META: This function does not alter the tabs
-   * @param {Array} tabs
-   */
-  set: function (tabs) {
-    // TODO:
-  },
-};
-
 function init () {
   $.getJSON('dummy-tabs.json', function (result) {
     // Generate the windows and add the .window property to each tab.
@@ -114,6 +45,10 @@ function init_search () {
   });
 }
 
+/**
+ * Evaluates the query and calls each script, passing its result onto the next.
+ * @param  {String} string
+ */
 function eval_query (string) {
   var arr = string.split('|').map(function (s) { return s.trim().replace(/\s\s+/g, ' '); });
   var result = cloneTabs(tabs);
@@ -136,29 +71,26 @@ function eval_query (string) {
 }
 
 /**
- * Gets the computed options form the set of arguments.
+ * Check if a keyboard event matches the given hotkey.
+ * IMPORTANT: You must use the NON-SHIFT value of a key (uppercase is ok, @ instead of 2 is not)
  *
- * Example: get_options(['yolo', '-a', '-x'], {vowel:['i', 'a'], consonants:['t', 'z', 'x']})
- *   -> {vowel: 'a', consonant: 'x'}
+ * Example: check_hotkey('ctrl+a', {ctrlKey:true, keyCode:97}) -> true;
  *
- * @param  {Array} args
- * @param  {Object} options See example.
- * @return {Object}
+ * @param  {String} str
+ * @param  {KeyboardEvent} event
+ * @return {Boolean}
  */
-function get_options (args, options) {
-  var result = {};
-  for (var key in options) { result[key] = options[key][0]; }
+function check_hotkey (str, event) {
+  var arr = str.split('+');
 
-  args.forEach(function (arg) {
-    if (arg[0] != '-') { return; }
-    arg = arg.slice(1);
-    for (var key in options) {
-      if (options[key].indexOf(arg) == -1) { continue; }
-      result[key] = arg;
-    }
-  });
+  for (var i=0; i<arr.length - 1; ++i) {
+    if (!event[arr[i].toLowerCase() + 'Key']) return false;
+  }
 
-  return result;
+  var code = arr[arr.length - 1].toUpperCase();
+  code = /^[0-9]+$/.test(code) ? parseInt(code) : code.charCodeAt(0);
+
+  return event.keyCode == code;
 }
 
 /**
@@ -208,6 +140,17 @@ function detach_event (name) {
 
   events[name][0].removeEventListener(events[name][1], events[name][2], false);
 }
+
+/**
+ * Send a message to the background page.
+ * @param  {String}   action
+ * @param  {Object}   data
+ * @param  {Function} callback
+ */
+function sendMessage (action, data, callback) {
+  return chrome.runtime.sendMessage({action:action, data:data}, callback || function () {});
+}
+
 
 /**
  * init()
